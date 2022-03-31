b32ee69d58b8c187d01533ae52ae4a07a3cdd789
JF <daniel-horne@virginmedia.com>
2022-03-31 23:37:02 +0100

    Update bbg.py

--- bbg.py
+++ bbg.py
@@ -1,1 +1,19 @@
-# This is where code will go!
+import pygame
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((1200, 800))
+pygame.display.set_caption('Blue Ball Game: Reballed')
+clock = pygame.time.Clock()
+
+
+
+# Main loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    pygame.display.update()
+    clock(30)
